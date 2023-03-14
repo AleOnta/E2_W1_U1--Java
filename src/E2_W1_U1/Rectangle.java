82b9ca73549cae7051c8e5f3c092d26cf262bda2
Alessandro Ontani <casualotn@gmail.com>
2023-03-14 15:33:05 +0100

    completed exercise 1 - task 1/2/3

--- src/E2_W1_U1/Rectangle.java
+++ src/E2_W1_U1/Rectangle.java
@@ -9,13 +9,38 @@ public class Rectangle {
 		height = h;
 	}
 	
-	public double getArea() {
+	public String getArea() {
 		double area = base * height;
-		return area;
+		return Double.toString(area);
 	}
 	
-	public double getPerimeter() {
+	public String getPerimeter() {
 		double perimeter = (base + height) * 2;
-		return perimeter;
+		return Double.toString(perimeter);
+	}
+	
+	public static void printRectangleData(Rectangle r) {
+		System.out.println("Area of R: " + r.getArea());
+		System.out.println("Perimeter of R: " + r.getPerimeter());
+	}
+	
+	public static void meltTwoRectangles(Rectangle r_1, Rectangle r_2) {
+		// printing in console data of first rectangle
+		double R1_area = Double.parseDouble(r_1.getArea());
+		double R1_perimeter = Double.parseDouble(r_1.getPerimeter());
+		System.out.print("Rectangle 1 Data: ");
+		System.out.print("Area: " + R1_area + ", ");
+		System.out.println("Perimeter: " + R1_perimeter);
+		// printing in console data of second rectangle
+		double R2_area = Double.parseDouble(r_2.getArea());
+		double R2_perimeter = Double.parseDouble(r_2.getPerimeter());
+		System.out.print("Rectangle 2 Data: ");
+		System.out.print("Area: " + R2_area + ", ");
+		System.out.println("Perimeter: " + R2_perimeter);
+		// calculating sum of area and perimeter
+		double totalArea = R1_area + R2_area;
+		double totalPerimeter = R1_perimeter + R2_perimeter;
+		System.out.println("Total area: " + totalArea);
+		System.out.println("Total perimeter: " + totalPerimeter);
 	}
 }
--- src/E2_W1_U1/myMainEx.java
+++ src/E2_W1_U1/myMainEx.java
@@ -3,8 +3,19 @@ package E2_W1_U1;
 public class myMainEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Task 1 Exercise 1 - creating 2 rectangles and calling their methods to calculate Area & Perimeter
+		Rectangle rectangle_A = new Rectangle(4.15, 8.9);
+		Rectangle rectangle_B = new Rectangle(2.4, 10.1);
+		System.out.print("R_A Area: " + rectangle_A.getArea() + ", ");
+		System.out.println("R_A Perimeter: " + rectangle_A.getPerimeter());
+		System.out.print("R_B Area: " + rectangle_B.getArea() + ", ");
+		System.out.println("R_B Perimeter: " + rectangle_B.getPerimeter());
+		
+		// Task 2 Exercise 1 - Calling a static function that return Area & Perimeter of rectangle passed as parameter
+		Rectangle.printRectangleData(rectangle_A);
+		
+		// Task 3 Exercise 1 - Calling a static function that return all data of two differents rectangles and sum Area & Perimeter of both
+		Rectangle.meltTwoRectangles(rectangle_A, rectangle_B);
 	}
 
 }
